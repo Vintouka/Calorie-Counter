764fab429b1c68f6aae1abe2282aae08eadf1c0d
Dylan Chuong <dylanvtchuong@gmail.com>
2025-10-05 17:48:50 -0700

    Removed unused import

--- app/src/main/java/com/example/caloriecounter/ui/Goals/GoalsFragment.java
+++ app/src/main/java/com/example/caloriecounter/ui/Goals/GoalsFragment.java
@@ -13,7 +13,6 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.caloriecounter.databinding.FragmentGoalsBinding;
 
